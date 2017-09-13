f2209bd7fbe2d414dd84552b785cd62e816a26e0
Ken Cochrane <kencochrane@gmail.com>
2017-09-13 15:12:54 -0400

    trying ELB variable again
    Signed-off-by: Ken Cochrane <kencochrane@gmail.com>

--- latest/swarm/infrakit.sh
+++ latest/swarm/infrakit.sh
@@ -16,11 +16,11 @@ alias infrakit='docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage
 
 {{ $groupsURL := cat (var `/infrakit/config/root`) `/groups.json` | nospace }}
 
-echo "ELB name = Var:{{ var `/cluster/elb/name` }}  Param:{{ var `/cluster/elb` }}"
+echo "ELB name = {{ var `/cluster/elb/name` }}"
 echo "Starting up infrakit  ######################"
 docker run -d --restart always --name infrakit -p 24864:24864 {{ $dockerMounts }} {{ $dockerEnvs }} \
        -e INFRAKIT_AWS_STACKNAME={{ var `/cluster/name` }} \
-       -e INFRAKIT_AWS_ELB_NAME={{ var `/cluster/elb` }} \
+       -e INFRAKIT_AWS_ELB_NAME={{ var `/cluster/elb/name` }} \
        -e INFRAKIT_AWS_METADATA_TEMPLATE_URL={{ var `/infrakit/metadata/configURL` }} \
        -e INFRAKIT_MANAGER_BACKEND=swarm \
        -e INFRAKIT_AWS_NAMESPACE_TAGS=infrakit.scope={{ var `/cluster/name` }} \
